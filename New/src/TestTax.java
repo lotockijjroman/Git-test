73a9adbc2519934cd9e4427f343b3124f439679e
roman <roman@lenovo>
2016-02-09 17:22:59 +0200

    I have just created new java project

---
+++ New/src/TestTax.java
@@ -0,0 +1,21 @@
+
+public class TestTax {
+
+	public static void main(String[] args) {
+		
+		//Tax t = new Tax();
+		NJTax t = new NJTax(); 
+		
+			t.grossIncome =40000;
+			t.numberOfDependents = 2;
+			t.state = "NJ";
+			
+			double yourTax =t.calcTax();
+			
+			double tjs = t.adjustForStudents(yourTax);
+			
+			System.out.println("Your tax is"  + " " + tjs);
+		}
+	}
+
+
